f75ff350b0c3c68c5b6213ebbd67617c810fa4f7
gabrielrez <gabrielrezcpessoa@gmail.com>
2024-07-27 08:53:13 -0300

    reatividade do sidebar componente

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState('Home');
+
+  const handleActive = (item) => {
+    setActiveItem(item);
+  };
+
   return (
     <div className="bg-c-gray-0 m-5 rounded-3xl text-center px-5 py-8 sidebar-custom-height hidden lg:block">
       <h1 className="text-2xl font-poppins font-bold mb-10 text-c-base-02">
@@ -8,46 +14,19 @@ const Sidebar = () => {
       </h1>
       <span className="block w-full h-0.5 bg-c-gray-1 my-12"></span>
       <ul className="space-y-5">
-        <li>
-          <a
-            href="#"
-            className="font-roboto font-normal text-white text-lg block w-60 py-4 bg-c-gray-1 rounded-full"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="font-roboto font-normal text-white text-lg block w-60 py-4 rounded-full transition duration-200 ease hover:bg-c-gray-1 rounded-full"
-          >
-            My Profile
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="font-roboto font-normal text-white text-lg block w-60 py-4 rounded-full transition duration-200 ease hover:border-c-gray-1 hover:bg-c-gray-1 rounded-full"
-          >
-            Analytics
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="font-roboto font-normal text-white text-lg block w-60 py-4 rounded-full transition duration-200 ease hover:border-c-gray-1 hover:bg-c-gray-1 rounded-full"
-          >
-            Settings
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="font-roboto font-normal text-white text-lg block w-60 py-4 rounded-full transition duration-200 ease hover:border-c-gray-1 hover:bg-c-gray-1 rounded-full"
-          >
-            Exit
-          </a>
-        </li>
+        {['Home', 'My Profile', 'Analytics', 'Settings', 'Exit'].map((item) => (
+          <li key={item}>
+            <a
+              href="#"
+              className={`font-roboto font-normal text-white text-lg block w-60 py-4 rounded-full transition duration-200 ease ${
+                activeItem === item ? 'bg-c-gray-1' : 'hover:bg-c-gray-1'
+              }`}
+              onClick={() => handleActive(item)}
+            >
+              {item}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
